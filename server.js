53fc10b1abfaad6b19b36aae31aeffeace69fb8b
dima0073231 <147907949+dima0073231@users.noreply.github.com>
2025-05-24 14:11:25 +0300

    add server js

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const bot = require('./bot.js'); 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+
+if (!process.env.TELEGRAM_BOT_TOKEN) {
+  console.error('❌ Токен бота не указан в .env!');
+  process.exit(1);
+}
+
+if (!WEBHOOK_URL) {
+  console.warn('⚠️ WEBHOOK_URL не задан, бот может работать нестабильно.');
+}
+
+app.use(express.json());
